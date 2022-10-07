922a1aced408e8e13b468865c72d7f8cec45bf2e
Sergio Morales García <moralesgarcia300@gmail.com>
2022-10-07 11:15:48 +0200

    Primer commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,12 +4,24 @@ import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 const routes: Routes = [
   {
     path: 'home',
-    loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)
+    loadChildren: () => import('./pages/home/home.module').then( m => m.HomePageModule)
   },
   {
     path: '',
     redirectTo: 'home',
     pathMatch: 'full'
+  },
+  {
+    path: 'persons',
+    loadChildren: () => import('./pages/persons/persons.module').then( m => m.PersonsPageModule)
+  },
+  {
+    path: 'tasks',
+    loadChildren: () => import('./pages/tasks/tasks.module').then( m => m.TasksPageModule)
+  },
+  {
+    path: 'assign',
+    loadChildren: () => import('./pages/assign/assign.module').then( m => m.AssignPageModule)
   },
 ];
 
